e5dfd62f1e6b5c679a1940367f0b467168046d2a
Joao Garrido <joao.b.garrido@gmail.com>
2024-03-01 10:31:46 -0300

    a

--- Erika/teste1.py
+++ Erika/teste1.py
@@ -89,7 +89,7 @@ def main():
             if medicamentos_filtrados.empty:
                 st.warning("Nenhum medicamento encontrado com o nome digitado.")
             else:
-                medicamentos_filtrados["Data de Validade"] = pd.to_datetime(medicamentos_filtrados["Data de Validade"])
+                medicamentos_filtrados["Data de Validade"] = pd.to_datetime(medicamentos_filtrados["Data de Validade"], errors='coerce')
 
                 st.write(medicamentos_filtrados.assign(**{"Data de Validade": medicamentos_filtrados["Data de Validade"].dt.strftime('%d-%m-%Y')}))
         else:
